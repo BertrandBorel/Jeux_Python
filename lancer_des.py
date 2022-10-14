dc01fce2f102e84178366adff9004a6d34e4543c
BertrandBorel <95342688+BertrandBorel@users.noreply.github.com>
2022-10-14 16:40:59 +0200

    Add files via upload

---
+++ lancer_des.py
@@ -0,0 +1,30 @@
+from random import randint
+
+class lancer_des :
+    def __init__(self, de):
+        self.de = de 
+
+    # Méthode pour lancer 1 seul dé
+    def lancer(self):
+        de = randint(1, 6)
+        print(f"-------------------------------------\n Résultat de votre dé : {de}")
+
+    # Méthode pour lancer 2 dés
+    def lancer_2(self):
+        de1 = randint(1, 6)
+        de2 = randint(1, 6)
+            # On affiche le résultat des dés
+        print(f"-------------------------------------\nVotre premier dé : {de1}")
+        print(f"Votre deuxième dé : {de2}")
+            # On fait le calcul de la somme totale
+        somme = de1 + de2 
+        print(f"Somme totale du lancer : {somme}")
+            # On retourne la valeur pour la stocker ?
+        return somme 
+         
+
+# Test : 
+lancer_des.lancer_2(lancer_des)
+lancer_des.lancer(lancer_des)
+
+
